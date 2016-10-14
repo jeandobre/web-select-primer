ee6c40091bc314ab3035a24efb637af573526787
jeandobre <jeandobre@gmail.com>
2016-10-14 11:31:38 -0400

    corrigido o envio de informações entre páginas e adicionado conversão de arquivo FASTA to txt.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,7 +3,7 @@ package controllers;
 import play.*;
 import play.mvc.*;
 //import play.data.Upload;
-//import java.io.File;
+import java.io.File;
 //import java.io.FileInputStream;
 
 //import models.*;
@@ -14,19 +14,31 @@ public class Application extends Controller {
     public static final String sequenciaParecida =  Play.configuration.getProperty("sequencia.parecida");
     public static final String sequenciaDiferente = Play.configuration.getProperty("sequencia.diferente");
 
+    public static final String localEntrada = Play.configuration.getProperty("diretorio.entrada");
+
     public static void index() {
         render();
     }
 
     public static void process(Parametros parametro){
-    /*    Logger.info(alfa.getContentType());
-    	Logger.info(alfa.getFieldName());
-    	Logger.info(alfa.getFileName());       
-        */
+        //validation.required(parametro);
+
+        //TODO validar o arquivo alfa
+        //tamanho
+        //caracteres ACGT
+        //FASTA
+        //System.out.println(parametro.alfa.getAbsolutePath());
+       // System.out.println(parametro.alfa.getCanonicalPath());
+        String parametroAlfa = localEntrada + parametro.alfa.getName();
+        ConvertFASTA2txt.converter(parametro.alfa, parametroAlfa);
 
 
+      //  arquivo.getFilename();
 
-        Logger.info(parametro.toString());
+
+       // System.out.println(parametro.alfa.getFileName());
+        //Logger.info(parametro.toString());
+
         render();
     }
 
--- app/controllers/Parametros.java
+++ app/controllers/Parametros.java
@@ -1,31 +1,40 @@
 package controllers;
 
+import play.data.validation.Min;
+import play.data.validation.Required;
 import play.mvc.*;
 import java.io.File;
 
 
-public class Parametros extends Controller {
-    File alfa;
-    File beta;
+public class Parametros{
+   @Required
+   public File alfa;
 
-    Integer k;
+   @Required
+   public File beta;
 
-    Boolean tipoProcessamento;
+   @Required
+   @Min(1)
+   public Integer k;
 
-    Integer j;
-    Integer distancia;
+   public Boolean tipoProcessamento;
 
-    Integer tipoKdiference; //1-parcedias, 2 - diferentes
+   @Min(0)
+   public Integer j;
+   public Integer distancia;
 
-    public java.lang.String toString() {
-        return "Parametros{" +
-                "arquivoAlfa='" + alfa + '\'' +
-                ", arquivoBeta='" + beta + '\'' +
-                ", k=" + k +
-                ", tipoProcessamento=" + tipoProcessamento +
-                ", j=" + j +
-                ", distancia=" + distancia +
-                ", tipoKdiference=" + tipoKdiference +
-                '}';
-    }
+   public Integer tipoKdiference; //1-parcedias, 2 - diferentes
+
+   @java.lang.Override
+   public java.lang.String toString() {
+      return "Parametros{" +
+              "alfa=" + alfa +
+              ", beta=" + beta +
+              ", k=" + k +
+              ", tipoProcessamento=" + tipoProcessamento +
+              ", j=" + j +
+              ", distancia=" + distancia +
+              ", tipoKdiference=" + tipoKdiference +
+              '}';
+   }
 }
